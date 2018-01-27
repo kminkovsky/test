44e252b0aa050100bc2f45e194d639d65a027334
kminkovsky <0oooo@interia.pl>
2018-01-27 11:34:55 +0100

    takie tam  śmieszki

---
+++ kol_PPR_2017_3b.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+ int k, n, m;
+ scanf("%d", &k);
+ int wynik;
+ for(int i=0; i<k; i++)
+ {
+ 	scanf("%d %d\n", &n, &m);
+ 	wynik = f(n,m);
+ 	printf("f(%d,%d)=%d\n", n, m, wynik);
+ }
+return 0;
+}
+	int f(int n, int m)
+	{
+		if(n != 0 && m==0)
+	{
+		return n;
+	}
+	if(n == 0 && m!=0)
+	{
+	return m;
+	}
+	if(n >= 0 && m>=0 && (m != 0 && n!= 0))
+	{
+		int odpowiedz = f((n-1), m) + f(n, m-1) + f(n-1, m-1);
+		return odpowiedz;
+	}
+	if(n==0 && m == 0)
+	{
+		return n;
+	}
+
+	}
+
